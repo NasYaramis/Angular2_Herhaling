091f33c84895cb7815c036f4d71f6982855e2bd1
Yaramis Naseyb (s103338) <naseyb.yaramis@student.ap.be>
2019-08-25 17:14:27 +0200

    added app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AddRecipeComponent } from './components/add-recipe/add-recipe.component';
+import { RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import { ListRecipeComponent } from './components/list-recipe/list-recipe.component';
+  
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddRecipeComponent,
+    ListRecipeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forChild([
+      {path: '', component: AddRecipeComponent},
+      {path: 'list', component: ListRecipeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
